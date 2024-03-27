31a0ec8d114ea5ed34a2da68f860a2182e88b9c8
Ryan <ryannolan1990@hotmail.com>
2024-03-27 13:42:04 +0000

    this will add the initial content to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-
+import patternDivider from "./images/pattern-divider-mobile.svg";
+import dice from "./images/icon-dice.svg";
 
 function App() {
+  return (
+    <div className="app-container">
+      <Advice />
+    </div>
+  );
+}
+
+function Advice() {
+  return (
+    <div className="advice-container">
+      <h2>ADVICE #117</h2>
+      <p>
+        “It is easy to sit up and take notice, what's difficult is getting up
+        and taking action.”
+      </p>
+      <div>
+        <img src={patternDivider} alt="Pattern Divider" />
+      </div>
+      <img src={dice} alt="Dice Icon" />
+    </div>
+  );
 }
 
 export default App;
